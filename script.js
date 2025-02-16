66dd5f06d2a449d48e02b14fa3a8d533c6042cb8
Anshika Dawar <anshikadawar4@gmail.com>
2025-02-16 22:05:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const hamburger =document.querySelector(".hamburger");
 
 const navLinks = document.querySelector(".nav-links");
 
+if(hamburger && navLinks) {
 hamburger.addEventListener("click" , () => {
 
     navLinks.classList.toggle("active");
-} );
+} );}
 
+// sections fadein on scroll
 document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll(".aboutus, .menu , .ambience , .reservation-section,   .customer-reviews");
+    const sections = document.querySelectorAll(".aboutus, .menu , .ambiance , .reservation-section,   .customer-reviews");
 
-    function handleScroll() {
+   if(sections.length > 0 ) function handleScroll() {
         sections.forEach((section) => {
             const sectionPosition = section.getBoundingClientRect().top;
             const screenPosition = window.innerHeight / 1.3;
@@ -28,35 +30,16 @@ document.addEventListener("DOMContentLoaded", function () {
     // Trigger animation when page loads
     handleScroll();
 });
-//Fade-in JavaScript -->
-  
-    document.addEventListener("DOMContentLoaded", function () {
-      const fadeSection = document.querySelector(".fade-in");
 
-      function fadeInOnScroll() {
-        const rect = fadeSection.getBoundingClientRect();
-        if (rect.top < window.innerHeight * 0.9) {
-          fadeSection.classList.add("visible");
-        }
-      }
-      window.addEventListener("scroll", fadeInOnScroll);
-      fadeInOnScroll();
-    });
   
-    
-// Handle contact form submission
-function handleContactFormSubmit(event) {
-  event.preventDefault(); // Prevent the form from submitting normally
-  alert('Thank you for your message! We will get back to you soon.');
-  event.target.reset(); // Reset the form
-}
-
-//resservation section
+    //resservation section popup
     document.addEventListener('DOMContentLoaded', function () {
       const reservationForm = document.getElementById('reservationForm');
       const popupMessage = document.getElementById('popupMessage');
       const closePopupBtn = document.querySelector('#popupMessage .popup-content button');
     
+     if(reservationForm && popupMessage && closePopupBtn) {
+      
       reservationForm.addEventListener('submit', function (event) {
         event.preventDefault(); // Prevent the form from submitting normally
     
@@ -71,4 +54,12 @@ function handleContactFormSubmit(event) {
         // Reset the form after closing the popup
         reservationForm.reset();
       });
+    }
     });
+
+// Handle contact form submission
+function handleContactFormSubmit(event) {
+  event.preventDefault(); // Prevent the form from submitting normally
+  alert('Thank you for your message! We will get back to you soon.');
+  event.target.reset(); // Reset the form
+}
